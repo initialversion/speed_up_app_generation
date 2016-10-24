099e2de9c41c4471456ae5ef6178a492e2ed3331
Raghu Betina <raghu@firstdraft.com>
2016-10-24 14:05:13 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "ptos#index"
   # Routes for the Pto resource:
   # CREATE
