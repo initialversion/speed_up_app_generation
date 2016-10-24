8dd9ad97446bde28eea3cce2a786700ef4b14fdc
Raghu Betina <raghu@firstdraft.com>
2016-10-24 14:05:24 +0000

    Whitelist columns in Pto for ActiveAdmin

--- app/admin/pto.rb
+++ app/admin/pto.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Pto do
 
+ permit_params :image, :caption, :owner_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
